0a359bce63f2f41e49af26fab9bc1e80c55310d2
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-10-13 20:25:28 +0530

    fetching data done

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,45 +1,21 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  async function getUsers() {
+    let response = await fetch("https://api.jsonbin.io/v3/b/63479f600e6a79321e26bb2b?meta=false");
+    let items = await response.json();
+    return items;
+  }
+  const promise = getUsers();
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div>
+  {#await promise}
+    <p>Loading...</p>
+  {:then items}
+    {#each items as item}
+      <p>Price is {item.price}, ItemName: {item.itemname}</p>
+    {/each}
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+</div>
+ */
